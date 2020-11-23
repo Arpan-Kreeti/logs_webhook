58f6503137619ba284dbee2389099fbbc5f375cb
Arpan <Arpan-Kreeti>
2020-11-23 21:48:31 +0530

    format json

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.scss"
+// import "../node_modules/pretty-print-json/pretty-print-json.js";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -16,6 +17,8 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
+import "./custom.js"
+
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
@@ -32,4 +35,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
